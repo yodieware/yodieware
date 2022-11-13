c9fcfd6fda4078aceae9a87eadc66da4f50ecd97
yodieware <118087165+yodieware@users.noreply.github.com>
2022-11-12 20:18:03 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/dahood.lua"))()
+elseif game.PlaceId == 10893141806 then
+    loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/truck_tycoon.lua"))()
+elseif game.PlaceId == 7305309231 then
+    loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/taxi_boss.lua"))()
+elseif game.PlaceId == 6814089265 then
+    loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/ragdoll.lua"))()
+elseif game.PlaceId == 10726371567 then
+    loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/simpsons.lua"))()
+end
