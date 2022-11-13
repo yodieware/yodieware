4539ffe0f2b55059f40c6fbe63335365116d4a47
yodieware <118087165+yodieware@users.noreply.github.com>
2022-11-13 18:58:15 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,4 +10,6 @@ elseif game.PlaceId == 10726371567 then
     loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/simpsons.lua"))()
 elseif game.PlaceId == 10925589760 then
     loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/merge%20sim.lua"))()
+elseif game.PlaceId == 10675042838 then
+    loadstring(game:HttpsGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/lemonade%20tycoon.lua"))()
 end
