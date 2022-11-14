e0abd590764a235e6799baf3a10452e94c7bac58
yodieware <118087165+yodieware@users.noreply.github.com>
2022-11-14 18:52:32 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,3 @@
 if game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/dahood.lua"))()
-    elseif game.PlaceId == 9825515356 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/hood%20customs.lua"))()
 end
