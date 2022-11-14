b65b44512b340314abfcabb7cefdd7f5525b251b
yodieware <118087165+yodieware@users.noreply.github.com>
2022-11-14 18:26:55 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,19 +1,19 @@
 if game.PlaceId == 2788229376 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/dahood.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/dahood.lua"))()
 elseif game.PlaceId == 10893141806 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/truck_tycoon.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/truck_tycoon.lua"))()
 elseif game.PlaceId == 7305309231 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/taxi_boss.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/taxi_boss.lua"))()
 elseif game.PlaceId == 6814089265 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/ragdoll.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/ragdoll.lua"))()
 elseif game.PlaceId == 10726371567 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/simpsons.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/simpsons.lua"))()
 elseif game.PlaceId == 10925589760 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/merge%20sim.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/merge%20sim.lua"))()
 elseif game.PlaceId == 10675042838 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/lemonade%20tycoon",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/lemonade%20tycoon"))()
 elseif game.PlaceId == 10675042838 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/slime%20tycoon.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/slime%20tycoon.lua"))()
 elseif game.PlaceId == 9825515356 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/hc.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yodieware/yodieware/main/games/hc.lua"))()
 end
